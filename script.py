da4a87c1ae461fe3bf312c5cdaa1b6152f4866a2
Ali21 <27100083@lums.edu.pk>
2026-06-04 02:33:05 +0500

    added more session details to avoid automatic security filtering

--- script.py
+++ script.py
@@ -20,7 +20,11 @@ BASE_URL = "https://zambeel.lums.edu.pk"
 def login():
     session = requests.Session()
     session.headers.update({
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/148.0.0.0"
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1'
     })
 
     login_page = session.get(f"{BASE_URL}/psp/ps/?cmd=login")
